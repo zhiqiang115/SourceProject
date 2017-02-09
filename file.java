bc177a85bf08cfc38849fd2b79165b4a6fb1e29a
zhiqiang115 <huangzhiqiang115@gmail.com>
2017-02-09 12:53:45 +0700

    Update file.java

--- file.java
+++ file.java
@@ -1,5 +1,10 @@
 // detech link url in string and insert to textview
 private SpannableString detectLinkURL(String data) {
+//	data = data.replaceAll("<a", "123@#");
+//	data = data.replaceAll("</a>", "124@#");
+//	data = data.replaceAll("<", "&lt;");
+//	data = data.replaceAll("123@#","<a");
+//	data = data.replaceAll("124@#","</a>");
 	Spanned text = Html.fromHtml(data);
 	//		Spanned text = Spannable.Factory.getInstance().newSpannable(data);
 	URLSpan[] currentSpans = text.getSpans(0, text.length(), URLSpan.class);
